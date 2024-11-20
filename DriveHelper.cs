ec2ab979bc11d60b40d17aec1265ce9d4d97a340
dmckittrick031 <dmckittrick031@wallacehigh.net>
2024-11-20 14:13:51 +0000

    Update DriveHelper.cs
    Removed unnecessary usings and added namespace declaration

--- DriveHelper.cs
+++ DriveHelper.cs
@@ -1,20 +1,20 @@
-using System.ComponentModel;
-using System.Drawing.Drawing2D;
-using System.Drawing.Printing;
 using System.Runtime.InteropServices;
 
-public static class DriveHelper
+namespace YourNamespace
 {
-    [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi)]
-    protected static extern int mciSendString(string lpstrCommand,StringBuilder lpstrReturnString, int uReturnLength, IntPtr hwndCallback);
-
-    public static void OpenCDROMDrive()
+    public static class DriveHelper
     {
-        mciSendString(“set cdaudio door open”, null, 0, IntPtr.Zero)
-    }
-
-    public static void CloseCDROMDrive()
-    {
-        mciSendString(“set cdaudio door close”, null, 0, IntPtr.Zero)
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi)]
+        protected static extern int mciSendString(string lpstrCommand,StringBuilder lpstrReturnString, int uReturnLength, IntPtr hwndCallback);
+    
+        public static void OpenCDROMDrive()
+        {
+            mciSendString(“set cdaudio door open”, null, 0, IntPtr.Zero)
+        }
+    
+        public static void CloseCDROMDrive()
+        {
+            mciSendString(“set cdaudio door close”, null, 0, IntPtr.Zero)
+        }
     }
 }
