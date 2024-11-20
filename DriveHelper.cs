275a9ea337008d56297b95698d9278526e0dbfbe
dmckittrick031 <dmckittrick031@wallacehigh.net>
2024-11-20 14:11:52 +0000

    Update DriveHelper.cs
    Changed methods to static to enable easier usage of the helper class

--- DriveHelper.cs
+++ DriveHelper.cs
@@ -3,17 +3,17 @@ using System.Drawing.Drawing2D;
 using System.Drawing.Printing;
 using System.Runtime.InteropServices;
 
-public class DriveHelper
+public static class DriveHelper
 {
     [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi)]
     protected static extern int mciSendString(string lpstrCommand,StringBuilder lpstrReturnString, int uReturnLength, IntPtr hwndCallback);
 
-    public void OpenCDROMDrive()
+    public static void OpenCDROMDrive()
     {
         mciSendString(“set cdaudio door open”, null, 0, IntPtr.Zero)
     }
 
-    public void CloseCDROMDrive()
+    public static void CloseCDROMDrive()
     {
         mciSendString(“set cdaudio door close”, null, 0, IntPtr.Zero)
     }
